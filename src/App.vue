afb81a786ad9f6caa7f4f29e10c3ab455a5c321e
MasoodahGeorge <oodasbusiness@gmail.com>
2024-07-05 07:53:59 +0200

    spinner

--- src/App.vue
+++ src/App.vue
@@ -1,90 +1,91 @@
 <template>
-  <!-- <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav> -->
-
   <div id="app">
-    <Navbar/>
-     <router-view></router-view>
+    <Navbar />
+    <div v-if="isLoading" class="spinner-container">
+      <Spinner :isLoading="isLoading"></Spinner>
+    </div>
+    <div v-else>
+      <router-view></router-view>
+      <section id="home-section">
+        <router-view name="home"></router-view>
+      </section>
+      <section id="about-section">
+        <router-view name="about"></router-view>
+      </section>
+      <section id="resume-section">
+        <router-view name="resume"></router-view>
+      </section>
+      <section id="skills-section">
+        <router-view name="skills"></router-view>
+      </section>
+      <section id="testimonials-section">
+        <router-view name="testimonials"></router-view>
+      </section>
+      <section id="projects-section">
+        <router-view name="projects"></router-view>
+      </section>
+      <section id="contact-section">
+        <router-view name="contact"></router-view>
+      </section>
+      <FooterComp />
+    </div>
   </div>
-  <!-- <router-view/> -->
-
-  <div id="app">
-    <section id="home-section">
-      <router-view name="home"></router-view>
-    </section>
-    <section id="about-section">
-      <router-view name="about"></router-view>
-    </section>
-    <section id="resume-section">
-      <router-view name="resume"></router-view>
-    </section>
-    <section id="skills-section">
-      <router-view name="skills"></router-view>
-    </section>
-    <section id="testimonials-section">
-      <router-view name="testimonials"></router-view>
-    </section>
-    <section id="projects-section">
-      <router-view name="projects"></router-view>
-    </section>
-    <section id="contact-section">
-      <router-view name="contact"></router-view>
-    </section>
-
-    <FooterComp/>
-  </div>
-
-
 </template>
 
 <script>
 import Navbar from './components/NavbarComp.vue';
-import FooterComp from './components/FooterComp.vue'
+import FooterComp from './components/FooterComp.vue';
+import Spinner from './components/SpinnerComp.vue'; // Adjust the path if needed
 
 export default {
   name: 'App',
   components: {
     Navbar,
-    FooterComp
+    FooterComp,
+    Spinner
+  },
+  data() {
+    return {
+      isLoading: true
+    };
+  },
+  mounted() {
+    // Simulate data loading with a timeout
+    setTimeout(() => {
+      this.isLoading = false;
+    }, 3000); // Adjust the timeout duration as needed
   }
 };
-
 </script>
 
 <style scoped>
-#about-section{
+#about-section {
   background-image: url('https://github.com/MasoodahGeorge/p3img/blob/main/Untitled%20design%20(13)%20copy.png?raw=true');
   height: 80vh;
 }
-#resume-section{
+#resume-section {
   background-image: url('https://github.com/MasoodahGeorge/p3img/blob/main/Untitled%20design%20(15).png?raw=true');
 }
-#skills-section{
+#skills-section {
   background-image: url('https://github.com/MasoodahGeorge/p3img/blob/main/0.png?raw=true');
 }
-#testimonials-section{
+#testimonials-section {
   background-image: url('https://github.com/MasoodahGeorge/p3img/blob/main/0.png?raw=true');
 }
-#projects-section{
+#projects-section {
   background-image: url('https://github.com/MasoodahGeorge/p3img/blob/main/3.png?raw=true');
 }
-#contact-section{
+#contact-section {
   background-image: url('https://github.com/MasoodahGeorge/p3img/blob/main/4.png?raw=true');
 }
 
 #app {
-  font-family:'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana, sans-serif;
+  font-family: 'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  background-color:#F3F3F3;
-  /* background-image: url('https://i.postimg.cc/XYTyBpTf/Untitled-design-8.png'); */
-  /* background-image: url('https://raw.githubusercontent.com/MasoodahGeorge/p3img/main/Untitled%20design%20(11).png'); */
-  /* background-size: cover; */
-  /* background-image: repeat; */
+  color: #2c4950;
+  background-color: #F3F3F3;
 }
 
 nav a {
@@ -92,7 +93,16 @@ nav a {
   color: #2c3e50;
 }
 
-/* nav a.router-link-exact-active {
-  color: #ea00ff;
-} */
+.spinner-container {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background: rgb(255, 132, 228); /* Optional: add a semi-transparent background */
+  z-index: 1000; /* Ensure it's above other content */
+}
 </style>
